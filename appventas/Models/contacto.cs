d81ce83ae355a10eb0efe5586f2428ce8132e0ff
Gianni caycho <caycho1797giannina@gmail.com>
2021-03-25 12:09:05 -0500

    primera parte semana 2

---
+++ appventas/Models/contacto.cs
@@ -0,0 +1,12 @@
+using System;
+namespace appventas.Models{
+
+public class contacto{
+    public string  Firstname {get;set;}
+    public string LastName {get;set;}
+    public string email {get;set;}
+    public string phone {get;set;}
+}
+
+
+}
