ec57b9183be50092ddec6bef2a907cc7767f0f5f
Gianni caycho <caycho1797giannina@gmail.com>
2021-03-25 19:35:05 -0500

    precio del producto

---
+++ producto/Controllers/ProductoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using producto.Models;
+
+namespace producto.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ILogger<ProductoController> _logger;
+
+        public ProductoController(ILogger<ProductoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       public IActionResult MetodoVenta(Producto objProducto){
+           double resultado = 0.0;
+           String  message ="";
+           String message2 ="";
+           resultado = objProducto.Precio * objProducto.Cantidad * 1.18;
+           message2= "el producto elegido es \n" + objProducto.Nombre;
+           message = "el precio del producto es \n" + resultado;
+           
+       ViewData["Messages"] = message2 + "\n";
+      ViewData["Message"] = message;
+      return View("index");
+      
+       }
+
+    }
+}
---
+++ producto/Models/Producto.cs
@@ -0,0 +1,9 @@
+using System;
+namespace producto.Models
+{
+    public class Producto{
+        public String Nombre{get; set;}
+        public double Precio{get;set;}
+        public double Cantidad{get;set;}
+    }
+}
